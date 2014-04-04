f3d224da082f149ec9f6ce47f5cc6d50cf35ba0b
DannyBenson <apprentice@dbc-chicago-006.local>
2014-04-04 18:44:58 -0500

    removes static pages controlle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,17 @@
 class UsersController < ApplicationController
+  before_action :logged_in?, only: [:hidden]
 
   def index
-
-      if user_signed_in?
-        @user = User.find(params[:id])
-      else
-        render_to root_path
-      end
+    if user_signed_in?
+      @user = User.find(params[:id])
+    else
+      render_to root_path
+    end
   end
 
   def sign_in
-      @user = User.new
-      # render partial: "devise/sessions/form"
-      render_to root_path
+    @user = User.new
+    render_to root_path
   end
 
   def show
