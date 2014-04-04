eb69bac93a5570cfe59fd1b8188628a0e5a752ee
Marco Morales <marco.bbcm@gmail.com>
2014-04-03 19:12:19 -0500

    Added comments controller spec file with pseudocode

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe CommentsController do
+  it "reloads the page after submitting a comment" do
+    # # create an image
+    # go to the image show page
+    # fill in comment
+    # expect response to be image show page
+  end
+
+  it "shows the comment text after the comment is submitted" do
+    # create an image
+    # go to the image show page
+    # fill in comment
+    # return to show page
+    # check page for comment text
+  end
+
+  it "shows the user email of the currently signed-in user" do
+    # create an image
+    # go to the image show page
+    # fill in comment
+    # submit the comment
+    # return to show page
+    # check page for current user email
+  end
+end
