2204852726cce170973eea6b37f23ad8b6e6bad3
DannyBenson <apprentice@dbc-chicago-006.local>
2014-04-04 07:59:40 -0500

    adds email to user comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @image = Image.find(params[:image_id])
+    # binding.pry
     @comment = @image.comments.create!(:text => params[:comment][:text],
                                        :user_id => current_user.id)
     redirect_to :back
