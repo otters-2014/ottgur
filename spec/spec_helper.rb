e75582bbf6d53328c5b837d5815bc1c1296fea2b
Apprentice <apprentice@dbc-chicago-003.local>
2014-04-05 16:19:06 -0500

    updated spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require 'coveralls'
 Coveralls.wear!('rails')
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'coveralls'
-Coveralls.wear!
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
