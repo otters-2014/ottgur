e441a84bbbbb326d676cc16cf0575c719d5155e4
DannyBenson <apprentice@dbc-chicago-006.local>
2014-04-05 16:39:29 -0500

    got test passing for favorite model get_image method

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -3,4 +3,14 @@ require 'spec_helper'
 describe Favorite do
   it { should belong_to(:user) }
   it { should belong_to(:image) }
+
+  describe "#get_image" do
+    let(:our_favorite) {Favorite.new(:image_id => 3)}
+
+    it "should return it's associated image object" do
+      Image.should_receive(:find).with(3)
+      our_favorite.get_image
+    end
+  end
+
 end
