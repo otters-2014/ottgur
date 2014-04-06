1c100396d3daa20372a389053278e232db575f43
DannyBenson <apprentice@dbc-chicago-006.local>
2014-04-05 20:07:21 -0500

    Profile page menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    user_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,14 @@ Devise::Application.routes.draw do
     resources :votes, only: [:create]
   end
 
+  get '/users/:user_id', to: redirect('/users/%{user_id}/favorites')
   post "/images/:image_id/favorites", to: "favorites#create", as: "image_favorites"
 
   resources :users do
     resources :favorites
   end
 
-
-
+# get '/stories', o: redirect('/posts')
   resources :users, only: [:show, :update], shallow: true do
     resources :images, only: [:show, :create, :delete, :index], shallow: true do
       resources :comments, shallow: true
