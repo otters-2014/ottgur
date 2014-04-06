84058f0a06f9140b7b637b9562000269533ac8cb
Apprentice <apprentice@dbc-chicago-009.local>
2014-04-06 18:57:33 -0500

    goof

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = Favorite.find_or_create_by(image_id: params[:image_id], user_id: current_user.id)
-    render git
+    render json: nil
   end
 
   def index
